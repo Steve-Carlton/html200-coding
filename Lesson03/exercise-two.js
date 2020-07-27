ceb360b0229dfca55fc1a427b7209aa6f8e9c747
Steve-Carlton <stevecarlton@Steves-MBP.lan>
2020-07-27 13:29:39 -0700

    Ex 2

---
+++ Lesson03/exercise-two.js
@@ -0,0 +1,14 @@
+/*Create a JavaScript file that does the following:
+
+Make a string variable of a sentence with least five words and a period.
+Make an array from that sentence and console.log the third word.
+Example:
+
+Bacon and eggs are delicious!
+// eggs*/
+
+let myString = 'How do you like them apples?';
+
+let arr = myString.split(' ');
+
+console.log(arr[2]);
