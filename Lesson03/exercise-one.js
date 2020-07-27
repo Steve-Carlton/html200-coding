f214739c921f556a88e3853a1cc03d9bf20e9082
Steve-Carlton <stevecarlton@Steves-MBP.lan>
2020-07-27 12:32:13 -0700

    lesson 3.1

---
+++ Lesson03/exercise-one.js
@@ -0,0 +1,20 @@
+/*Create a JavaScript file that does the following:
+
+Do a console log after each item
+• Create an array, with at least 5 strings
+• Add an element to the end of the array
+• Remove the third element
+• Create a string from the elements and comma separate them*/
+
+let myArray = ['earth', 'wind', 'fire', 'water', 'heart']; 
+console.log(myArray);
+
+myArray.push('With your powers combined...');
+console.log(myArray);
+
+myArray.splice(2, 2);
+console.log(myArray);
+
+let myString = myArray.join(', ');
+console.log(myString);
+  
