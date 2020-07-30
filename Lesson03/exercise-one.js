65f388accc7e3a30419a9c66cdc1609d58ab4efa
Steve-Carlton <stevecarlton@Steves-MBP.lan>
2020-07-30 11:19:45 -0700

    splice fix

--- Lesson03/exercise-one.js
+++ Lesson03/exercise-one.js
@@ -6,15 +6,14 @@ Do a console log after each item
 • Remove the third element
 • Create a string from the elements and comma separate them*/
 
-let myArray = ['earth', 'wind', 'fire', 'water', 'heart']; 
+let myArray = ['earth', 'wind', 'fire', 'water', 'heart'];
 console.log(myArray);
 
 myArray.push('With your powers combined...');
 console.log(myArray);
 
-myArray.splice(2, 2);
+myArray.splice(2, 1);
 console.log(myArray);
 
 let myString = myArray.join(', ');
 console.log(myString);
-  
