372acd2456423085f40015eb84a9b2ab55c2f21e
Steve-Carlton <63474298+Steve-Carlton@users.noreply.github.com>
2020-08-14 13:01:05 -0700

    initial greetPrompt

--- Lesson05/scripts.js
+++ Lesson05/scripts.js
@@ -1,4 +1,33 @@
 //Initial User Prompt
 function greetPrompt() {
-  prompt('Welcome valued customer. Select an action: Enter W to withdraw. D to deposit. B to view balance. Q to Quit.');
+  prompt('Welcome valued customer. Enter W to withdraw. D to deposit. B to view balance. Q to Quit.');
+
+}
+
+
+//User Input Flow Control
+let userInput;
+
+function mainMenu(greetPrompt) {
+  switch (greetPrompt) {
+      case 'Q': {
+        console.log('Goodbye.');
+      }
+      case 'W': {
+        //return ~make a withdrawl
+        break;
+      }
+      case 'D': {
+        //return ~make a deposit
+        break;
+      }
+      case 'B': {
+        //return ~display bank balance
+        break;
+      }
+    default: {
+      console.log('Invalid entry');
+      greetPrompt()
+    }
+  }
 }
