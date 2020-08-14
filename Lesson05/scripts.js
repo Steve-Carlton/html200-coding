2c2cf514cf7fd091d9218fae3414f41ac51ce5e9
Steve-Carlton <63474298+Steve-Carlton@users.noreply.github.com>
2020-08-14 15:17:17 -0700

    mainMenu switch in place

--- Lesson05/scripts.js
+++ Lesson05/scripts.js
@@ -1,17 +1,31 @@
 //Main Menu Flow Control
-let userInput;
+let userInput = prompt('Welcome valued customer. Enter W to withdraw. D to deposit. B to view balance. Q to Quit.');
 
 function mainMenu(userInput) {
-prompt('Welcome valued customer. Enter W to withdraw. D to deposit. B to view balance. Q to Quit.');
   switch (userInput) {
-      case 'Q':
-      case 'W':
-      case 'D':
-      case 'B': {
-      console.log('bananas.');
+    case 'Q':
+    case 'q': {
+      console.log('quit');
       break;
-      }
-    //default: {
-      //console.log('Invalid entry');
+    }
+    case 'W':
+    case 'w': {
+      console.log('withdraw');
+      break;
+    }
+    case 'D':
+    case 'd': {
+      console.log('deposit.');
+      break;
+    }
+    case 'B':
+    case 'b': {
+      console.log('balance');
+      break;
+    }
+    default: {
+      console.log('invalid entry');
+      alert('invalid entry');
+    }
   }
 }
